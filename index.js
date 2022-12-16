fc1837ebfcae74620e70cf7ae38540ab88addc32
vlopeznadal <vlopeznadal@icloud.com>
2022-12-16 14:39:46 -0600

    Restructured into functions

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
-"/"];
+import { characters } from '/data.js'
 
 let passwordButton = document.getElementById("password-btn")
-let passDisplay1 = document.getElementById("password1")
-let passDisplay2 = document.getElementById("password2")
 
 passwordButton.addEventListener("click", function() {
+    generatePasswords()
+})
+
+function generatePasswords() {
     let password1 = ""
     let password2 = ""
 
@@ -14,9 +15,15 @@ passwordButton.addEventListener("click", function() {
     let index2 = Math.floor(Math.random()*91)
     password1 += characters[index1]
     password2 += characters[index2]
+    }
+
+    displayPasswords(password1,password2)
 }
 
-passDisplay1.textContent = password1
-passDisplay2.textContent= password2
+function displayPasswords(password1, password2) {
+    let passDisplay1 = document.getElementById("password1")
+    let passDisplay2 = document.getElementById("password2")
 
-})
+    passDisplay1.textContent = password1
+    passDisplay2.textContent= password2
+}
